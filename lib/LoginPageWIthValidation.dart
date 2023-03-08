184daf1282597dde641d19d95e0e81670a54e2fa
JustinNThampi <justinthampy464@gmail.com>
2023-03-08 13:41:17 +0530

    FirdtCommit

--- lib/LoginPageWIthValidation.dart
+++ lib/LoginPageWIthValidation.dart
@@ -13,7 +13,69 @@ class Login_With_Validation extends StatefulWidget {
 class _Login_With_ValidationState extends State<Login_With_Validation> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
-    
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Page'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Image.asset(
+                'assets/icons/profile1.png',
+                width: 200,
+                height: 200,),
+            ),
+            
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.account_box_sharp),
+                  hintText: 'Username',
+                  labelText: 'Username',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(100))
+                ),
+              ),
+            ),
+            
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                obscureText: true,
+                obscuringCharacter: '*',
+                decoration: InputDecoration(
+                  helperText: 'Password length should be greater than 6',
+                  prefixIcon: Icon(Icons.password),
+                  suffixIcon: Icon(Icons.visibility_off),
+                  hintText: 'Password',
+                  labelText: 'Password',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(100))
+                ),
+              ),
+            ),
+            
+            Padding(
+              padding: const EdgeInsets.only(top: 30,bottom: 30),
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text('Login')),
+            ),
+            
+            TextButton(
+              onPressed: (){},
+              child: Text(
+                'Not a User?? SignUp Here!!',
+                style: TextStyle(
+                fontSize: 10),))
+          ],),
+      ),
+    );
   }
+  
 }
