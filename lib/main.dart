1fa9942413368ab37555338b094ecceab039c01f
JustinNThampi <justinthampy464@gmail.com>
2023-03-15 19:46:02 +0530

    latest commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'HomeScreen.dart';
+import 'Login_Signup_UI/Splash.dart';
 import 'SplashPage.dart';
 import 'Login_Page.dart';
 import 'LoginPageWIthValidation.dart';
@@ -12,7 +13,8 @@ void main(){
       //home : Splashh(),
       //home : LoginPage(),
       //home : Login_With_Validation(),
-      home : HomeScreen(),
+      //home : HomeScreen(),
+      home: Splash(),
       debugShowCheckedModeBanner: false,
     ),
   ));
